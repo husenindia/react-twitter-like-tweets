e302749a3c76912b98a51ed37b3ab67afcd7d6c3
husen.telwala <husentelwala@gmail.com>
2026-06-09 18:32:39 +0530

    Main header with action added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
 import PostList from './componets/post-list/PostList'
+import MainHeader from './componets/main-header/MainHeader'
+import { useState } from 'react'
 function App() {
+
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler(event) {
+    setModalIsVisible(true);
+  }
+  function closeModalHandler(event) {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <PostList></PostList>
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler}></MainHeader>
+      <main>
+        <PostList 
+        onCloseModalFunc={closeModalHandler}
+        modalIsVisibleProp={modalIsVisible}></PostList>
+      </main>
+    </>
   )
 }
 
