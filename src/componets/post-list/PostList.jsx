608dc937f247a8a6fb59475111a53f06834f5c04
husen.telwala <husentelwala@gmail.com>
2026-06-09 18:32:06 +0530

    Post list added

--- src/componets/post-list/PostList.jsx
+++ src/componets/post-list/PostList.jsx
@@ -1,12 +1,34 @@
 
+import { useState } from 'react';
 import NewPost from '../new-post/NewPost';
 import Post from '../post-card/Post';
 import classes from './PostList.module.css'
-function PostList() {
+import Modal from '../modal/Modal';
+function PostList({modalIsVisibleProp, onCloseModalFunc}) {
+    const [postBody, setPostBody] = useState("");
+    const [postAuthor, setPostAuthor] = useState("");
+
+
+    function changePostAuthorHandler(event) {
+        setPostAuthor(event.target.value);
+    }
+
+    function changePostBodyHandler(event) {
+        setPostBody(event.target.value);
+    }
+
     return (
         <>
-            <NewPost/>
+            
+            {modalIsVisibleProp && 
+                <Modal onCloseModalFunc={onCloseModalFunc}>
+                    <NewPost 
+                    onPostBodyChange={changePostBodyHandler}
+                    onPostAuthorChange={changePostAuthorHandler} />
+                </Modal> 
+            }   
             <ul className={classes.posts}>
+                <li><Post postAuthor={postAuthor} postContent={postBody} /></li>
                 <li><Post postAuthor="Husen" postContent="Post Content by Husen" /></li>
                 <li><Post postAuthor="Fatema" postContent="Post Content by Fatema" /></li>
                 <li><Post postAuthor="Zehara" postContent="Post Content by Zehara" /></li>
